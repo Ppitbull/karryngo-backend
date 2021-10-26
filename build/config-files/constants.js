ddd394a525d4087580e938293b524dbb6425e8a5
Cédric Nguendap <cednguendap@gmail.com>
2021-10-26 22:32:08 +0200

    update tsconfig

--- build/config-files/constants.js
+++ build/config-files/constants.js
@@ -1,12 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var Configuration = {
-    "app_config_file": "./config-files/app.json",
+    "app_config_file": "\\config-files\\app.json",
     "file_config_type": 'json',
-    'class_for_configuration': "/karryngo_core/config/JsonFileConfigurationService",
-    'path_for_module': "/karryngo_module",
-    'path_for_bussiness_module': "/karryngo_modules/bussiness",
-    'path_for_bussiness_service': "/karryngo_modules/services",
+    'class_for_configuration': "\\karryngo_core\\config\\JsonFileConfigurationService",
+    'path_for_module': "\\karryngo_module",
+    'path_for_bussiness_module': "\\karryngo_modules\\bussiness",
+    'path_for_bussiness_service': "\\karryngo_modules\\services",
     'collections': {
         'notification': 'Notifications',
         'user': 'Users',
