af9c6ac138e7969d18f34ef26daa1808729a9331
Cédric Nguendap <cednguendap@gmail.com>
2022-03-07 10:21:59 +0100

    optimizing starting time in setup process

--- app.ts
+++ app.ts
@@ -12,43 +12,33 @@ let router=express.Router();
 const httpServer = require("http").createServer(app);
 var timeout = require('connect-timeout')
 
-function haltOnTimedout(req, res, next) {
-    if (!req.timedout) next()
-}
 app.use(timeout('120s'))
 
 app.use(cors())
-app.use(haltOnTimedout)
-//instanciation du coeur de Karryngo
-let karryngoApp = InjectorContainer.getInstance().getInstanceOf<KarryngoApp>(KarryngoApp)
-karryngoApp.init(router,httpServer,app);
-app.use(((request:any,response:any,next:any)=>
-{
-    karryngoApp.run();
 
-    next();
-}));
-
-app.use(haltOnTimedout)
 //ceci permet de gérer les tailles des json en entrée très grand
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
-//ceci permet de gérer les tailles des json en entrée très grand
-app.use(bodyParser.json({limit: '50mb'}));
 
 //utilisation du router
 app.use(router);
-app.use(haltOnTimedout)
+
 // Setup server port
 var port = process.env.PORT || 8090;
 
 // Send message for default URL 
 app.use(express.json())
 app.get('/', (req:any, res:any) => res.send('Hello World with Express'));
-app.use(haltOnTimedout)
 
-// Use Api routes in the App
-//app.use('/api', apiRoutes)
+//instanciation du coeur de Karryngo
+let karryngoApp = InjectorContainer.getInstance().getInstanceOf<KarryngoApp>(KarryngoApp)
+karryngoApp.init(router, httpServer, app);
+app.use(((request: any, response: any, next: any) => {
+    karryngoApp.run();
+
+    next();
+}));
+
 
 // Launch app to listen to specified port
 httpServer.listen(port, function () {
